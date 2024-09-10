bcdf9d252ab66b3fe007fca75677c0d050c95a53
FannyFalck <falckfanny@gmail.com>
2024-09-10 19:59:42 +0200

    test 1 carousel
    Test for carusell

--- src/script.js
+++ src/script.js
@@ -46,3 +46,18 @@ function TimeCountDown() {
 }
 
 TimeCountDown();
+
+// ******* ******* Carusell  ******* ******* 
+document.addEventListener('DOMContentLoaded', function() {
+  const swiper = new Swiper('.swiper-container', {
+    loop: false,  // Stäng av loop-läget
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+});
